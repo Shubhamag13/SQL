77ce7e27b01bb6588b062b109c202889eefa7631
Shubham Agrawal <agrawalshubham667@gmail.com>
2026-03-13 16:16:13 +0530

    Delete Distinct.java

--- Distinct.java
+++
@@ -1,5 +0,0 @@
--- DISTINCT() --> only receive distinct value (find to unique value from one or more column we use distinct)
- SELECT distinct (continent) FROM country;
- SELECT distinct continent, region FROM country;
- 
- -- SUM() , AVG()
